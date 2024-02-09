04ec1d1c68606b9042b852726264c2908560c731
Dennis <57582915+Dennis824@users.noreply.github.com>
2024-02-09 17:21:26 -0600

    Initial commit

---
+++ src/main/java/com/example/springhtmx/model/State.java
@@ -0,0 +1,7 @@
+package com.example.springhtmx.model;
+
+public record State(Integer id,
+                    String userHandle,
+                    String userDisplayName,
+                    String content) {
+}
